e08b695027fe99e00a0d8a5aec4d9b6bee112235
qinxij <qinxij@163.com>
2016-02-27 23:49:14 +0800

    Passing python variables to be rendered in the templete

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.template.backends.utils import csrf_token_lazy
 
 from lists.views import home_page
 
@@ -19,5 +20,25 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
 
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string(
+            'home.html',
+            {
+                'csrf_token': csrf_token_lazy(request),
+            })
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {
+                'csrf_token': csrf_token_lazy(request),
+                'new_item_text': 'A new list item'
+            })
         self.assertEqual(response.content.decode(), expected_html)
